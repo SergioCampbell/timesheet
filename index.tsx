9c32031c949eb6ce47b621bfc30c7fc6ee691d66
Sergio Campbell Dev <51247757+SergioCampbell@users.noreply.github.com>
2022-11-15 20:25:53 -0500

    Delete index.tsx

--- index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
-import App from "./app";
-
-import './styles.scss'
-
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<App />);
